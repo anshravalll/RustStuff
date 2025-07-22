ac8ac87f79b39168ddf9ef16f73edab137ac21f1
anshravalll <anshraval@gmail.com>
2025-07-22 23:56:12 +0530

    learning ownership

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -8,21 +8,14 @@
 //  4. Is it accessible inside the new function? Yes, always, in all of the below scenarios, it is.
 //     Because the function is having the accessibility of the variable.
 
-
-
-
-
-
-
-
-//Heap based 
-    //With mut
-        //With ownership
+//Heap based
+//With mut
+//With ownership
 // fn foo(mut s: String) -> String{
 //     println!("Bruh, thisi s your thing? {s}");
 //     s.push_str("Bye, world!!");
 //     s
-// } //This function: Accesses, and returns the variable but doesn't modify it. 
+// } //This function: Accesses, and returns the variable but doesn't modify it.
 //
 // fn main(){
 //     let mut s = String::from("Hello, World!!"); //Heap based allocation
@@ -35,8 +28,8 @@
 // 3. Well, no, because the answer of 1. is No
 
 // MISPLACED
-    //With ownership
-    //Immutable
+//With ownership
+//Immutable
 // fn foo(s: &String) -> String{ //& doesn't work here, as it is purely for reference (borrowing)
 // purposes.
 //
@@ -48,18 +41,12 @@
 // fn main(){
 //     let s = String::from("Hello, World!!"); //Heap based allocation
 //     foo_returns = foo(s); //Pure ownership transfer without any reference
-//     println!("{s}"); //
+//     println!("{s}");
 //     println!("Same s that foo returns is: {foo_returns}");
 // }
 
-
-
-
-
-
-
-        //With borrowing
-            //with reference 
+//With borrowing
+//with reference
 
 // fn foo(s: &String) {
 //     let mut s = String::from("Unexpected");
@@ -76,21 +63,18 @@
 
 // 1. Yes, because there is no transfer of ownership just references
 // 2. No, the condition to allow variable to change data for heap based data is to have it mutable and then to have its mutable reference rather than the immutable one.
-//
-            
+// (maybe above all answers are sufficient)
 
-        //With borrowing
-            //with mut
-                //with reference 
+//With borrowing
+//with mut
+//with reference
 
-
-
-fn foo(s: &mut String) -> &String{
+fn foo(s: &mut String) -> &String {
     println!("Hey this is raw s from foo function, s: {s}");
     s = String::from("Bye, World!!!");
     s
 }
-fn main(){
+fn main() {
     let mut x = String::from("Hello, World!!");
     let foo_returns = foo(&mut x);
 
