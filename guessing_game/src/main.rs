ac3cc399831dca7dbf673b37e35ad1731f1e0538
anshravalll <anshraval@gmail.com>
2025-08-24 23:53:29 +0530

    doc

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -4,6 +4,7 @@ use std::cmp::Ordering;
 
 use rand::Rng;
 
+//classic, directly from rust book
 fn main() {
     println!("Guess the number!!!!");
 
@@ -13,18 +14,18 @@ fn main() {
         println!("please input your guess");
 
         let mut guess = String::new();
-    
+
         io::stdin()
             .read_line(&mut guess)
             .expect("Failed to read line");
-    
-        let guess: u32 = match guess.trim().parse(){
+
+        let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
             Err(_) => continue,
         };
-    
+
         println!("You guessed: {}", guess);
-    
+
         match guess.cmp(&secret_no) {
             Ordering::Less => println!("Too small"),
             Ordering::Greater => println!("Too big"),
